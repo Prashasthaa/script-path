6ffe77ea325411ded3d3dae42a4049a4ca5811ca
Prashastha <poojaryprashastha@gmail.com>
2025-01-29 19:10:41 +0530

    Challenge-2
    Strings & literals

--- Fundamentals-part-1/script.js
+++ Fundamentals-part-1/script.js
@@ -121,4 +121,20 @@ if (isOldEnough) {
   console.log(`Sarah is to young. wait another ${yearsLeft} years :)`);
 }
 
+//CHALLENGE-2
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+//BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+
+console.log(BMIMark, BMIJohn);
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's  BMI (${BMIMark}) is higher than John (${BMIJohn})`);
+} else {
+  console.log(`John's  BMI (${BMIJohn}) is higher than Mark (${BMIMark})`);
+}
 

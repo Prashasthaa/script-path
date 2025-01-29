f76da29bb00bdf3f65e365b2365e2730bb414fb1
Prashastha <poojaryprashastha@gmail.com>
2025-01-29 14:48:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+//values
+console.log("Prashastha"); //prashastha is a value
+console.log(23);
+
+//variables
+let firstName = "Prashastha"; //firstName is variable
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//variable name convientions
+let PI = 3.14157;
+
+let myFirstJob = "Programmer";
+let myCurentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+
+//single line comment
+/* Multiline comment */
+
+//boolean values
+console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+//type off
+console.log(typeof true); //boolean
+console.log(typeof javaScriptIsFun); //boolean
+console.log(typeof 23); //number
+console.log(typeof "Hello"); //string
+
+//Dynamic typing
+javaScriptIsFun = "yes";
+console.log(typeof javaScriptIsFun); //string
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year); //undefined
+
+//null
+console.log(null);

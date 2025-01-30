4848e17ab7b56d9d5b9a352d0fdef8999b03b447
Prashastha <poojaryprashastha@gmail.com>
2025-01-30 15:25:16 +0530

    challenge-1

--- Fundamentals-part-2/script.js
+++ Fundamentals-part-2/script.js
@@ -46,3 +46,41 @@ console.log(age1, age2);
 const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(2003);
 console.log(age1, age2, age3);
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+//challenge-1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+//test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 ( ${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log("No team wins ...");
+  }
+};
+checkWinner(scoreDolphins, scoreKoalas);
+
+//test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+checkWinner(scoreDolphins, scoreKoalas);

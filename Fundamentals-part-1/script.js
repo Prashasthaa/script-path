40a42b8f8e330ad092ae8f62ad535dd2725b232f
Prashastha <poojaryprashastha@gmail.com>
2025-01-30 07:16:00 +0530

    Challe

--- Fundamentals-part-1/script.js
+++ Fundamentals-part-1/script.js
@@ -174,4 +174,20 @@ console.log(hasDriverLicense || hasgoodVision); //true
 console.log(!hasDriverLicense); //false
 console.log(!hasgoodVision); //true
 
+//challenge-3
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100)
+  console.log("Dolphins win the trophy🏆");
+else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100)
+  console.log("Koalas win the trophy🏆");
+else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+)
+  console.log("Both win the trophy🏆");
+else console.log("No one wins trophy 😒");
 

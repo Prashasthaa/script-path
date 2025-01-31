244a13ff8761c3ee88cac57e4ad2d5fd7fa45012
Prashastha <poojaryprashastha@gmail.com>
2025-01-31 15:11:19 +0530

    challenge-3

--- Fundamentals-part-2/script.js
+++ Fundamentals-part-2/script.js
@@ -213,3 +213,75 @@ if (mark.bmi > john.bmi) {
     `${mark.fullName}'s BMI(${mark.bmi}) is lesser than ${john.fullName}'s BMI(${john.bmi})`
   );
 }
+
+//loops
+for (let i = 1; i <= 10; i++) {
+  console.log(`Lifting weights repetition ${i} 🏋️`);
+}
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Micheal", "Peter", "Steven"],
+];
+// for (let i = 0; i <= jonas.length - 1; i++) {
+//   console.log(jonas[i]);
+// }
+
+//continue
+console.log("--Only strings---");
+for (let i = 0; i <= jonas.length - 1; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+//break
+console.log("--BREAK---");
+for (let i = 0; i <= jonas.length - 1; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+//loops in loop
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`----Starting Exercise ${exercise}-----`);
+  for (let i = 1; i <= 5; i++) {
+    console.log(`Lifting weights repetition ${i} 🏋️`);
+  }
+}
+
+//while loop
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weights repetition ${rep} 🏋️`);
+  rep++;
+}
+
+////challenge-4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //sum = sum + arr[i];
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));

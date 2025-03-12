48e69c9e2abd15d8547c4374e0ce2bf12ff63515
Prashastha <poojaryprashastha@gmail.com>
2025-03-12 20:09:39 +0530

    update

--- 02-Fundamentals-part-2/script.js
+++ 02-Fundamentals-part-2/script.js
@@ -401,19 +401,28 @@
 //   friends: ["Micheal", "Steven", "Peter"],
 // };
 
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Pojary",
-  age: 2037 - 1996,
-  job: "Teacher",
-  friends: ["Micheal", "Steven", "Peter"],
-};
-console.log(jonas);
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Pojary",
+//   age: 2037 - 1996,
+//   job: "Teacher",
+//   friends: ["Micheal", "Steven", "Peter"],
+// };
+// // console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas["lastName"]);
+// // console.log(jonas.lastName);
+// // console.log(jonas["lastName"]);
 
-//challenge
-console.log(
-  `${jonas.firstName} has ${jonas.friends.length} friends , and his bestfriend is ${jonas.friends[0]}`
-);
+// // //challenge
+// // console.log(
+// //   `${jonas.firstName} has ${jonas.friends.length} friends , and his bestfriend is ${jonas.friends[0]}`
+// // );
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Pojary",
+//   age: 2037 - 1996,
+//   job: "Teacher",
+//   friends: ["Micheal", "Steven", "Peter"],
+// };
+// console.log(jonas);

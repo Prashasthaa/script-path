93eea001815eb853f0b00193f220efe373fc6f93
Prashastha <poojaryprashastha@gmail.com>
2025-06-01 23:30:27 +0530

    update

---
+++ Java/Java-Programs/average.java
@@ -0,0 +1,9 @@
+public class average {
+    public static void average(int a, int b, int c) {
+        System.out.println((a + b + c) / 3);
+    }
+
+    public static void main(String args[]) {
+        average(4, 5, 6);
+    }
+}

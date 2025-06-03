192be95824eee9bf5b26b8c0276ea8dac34dfeca
Prashastha <poojaryprashastha@gmail.com>
2025-06-03 16:33:29 +0530

    update

---
+++ Java/Java-Programs/ArrayCC.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArrayCC {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        // int numbers[] = { 1, 2, 3 };
+
+        // String fruits[] = { "apple", "orange" };
+
+        Scanner sc = new Scanner(System.in);
+
+        // int phy;
+        // phy = sc.nextInt();
+
+        System.out.println("Length of array : " + marks.length);
+
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
+
+        // System.out.println("Phy : " + marks[0]);
+        // System.out.println("chem : " + marks[1]);
+        // System.out.println("Math : " + marks[2]);
+
+        // sc.close();
+    }
+}

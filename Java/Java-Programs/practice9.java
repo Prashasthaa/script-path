77d1cb12c38e4c0fa38c7ec17e1f45be9c85aa70
Prashastha <poojaryprashastha@gmail.com>
2025-06-06 19:26:06 +0530

    update

--- Java/Java-Programs/practice9.java
+++ Java/Java-Programs/practice9.java
@@ -13,9 +13,26 @@ public class practice9 {
         return flag;
     }
 
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        for (int j = 1; j <= k; j++) {
+            int temp = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                nums[i + 1] = nums[i];
+            }
+            nums[0] = temp;
+        }
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void main(String[] args) {
         int nums[] = { 1, 2, 3, 15 };
-        System.out.println(distinct(nums));
+        int k = 14;
+        // System.out.println(distinct(nums));
+
+        rotate(nums, k);
 
     }
 }

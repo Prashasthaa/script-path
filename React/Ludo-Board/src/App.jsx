620e89690a328076d8e295b1c8316841132394a9
Prashastha <poojaryprashastha@gmail.com>
2025-08-07 06:34:30 +0530

    update

--- React/Ludo-Board/src/App.jsx
+++ React/Ludo-Board/src/App.jsx
@@ -5,11 +5,17 @@ import "./App.css";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
 import LotteryTicket from "./LotteryTicket";
+import TicketNum from "./TicketNum";
+import Ticket from "./Ticket";
+import { sum } from "./helper";
 
 function App() {
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  };
   return (
     <>
-      <LotteryTicket />
+      <LotteryTicket n={3} winCondition={winCondition} />
     </>
   );
 }
--- React/Ludo-Board/src/LotteryTicket.jsx
+++ React/Ludo-Board/src/LotteryTicket.jsx
@@ -1,26 +1,21 @@
 import { useState } from "react";
-import "./LotteryTicket.css";
-import { genTicket } from "./helper";
-export default function LotteryTicket() {
-  let [ticket, setTicket] = useState(genTicket(3));
-  let generateNum = () => {
-    setTicket(genTicket(3));
-  };
+import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
+import Button from "./Button";
 
+export default function LotteryTicket({ n = 3, winCondition }) {
+  let [ticket, setTicket] = useState(genTicket(n));
+  let generateNum = () => {
+    setTicket(genTicket(n));
+  };
+  let isWin = winCondition(ticket);
   return (
     <div>
       <h1>Lottery</h1>
-      <h2>
-        {" "}
-        {ticket[0] + ticket[1] + ticket[2] == 15 ? "Congratulations 🎉" : ""}
-      </h2>
-      <div className="ticket">
-        <span>{ticket[0]}</span>
-        <span>{ticket[1]}</span>
-        <span>{ticket[2]}</span>
-      </div>
-      <br />
-      <button onClick={generateNum}>Generate new number</button>
+
+      <Ticket ticket={ticket} />
+      <Button action={generateNum} />
+      <h2>{isWin && "Congratulations 🎉"}</h2>
     </div>
   );
 }

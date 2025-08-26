6afc2b85c469c3178bc2a98ebe7db38bd8f1d9ec
Prashastha <poojaryprashastha@gmail.com>
2025-08-26 17:23:59 +0530

    update

--- Java/Java-Programs/BitManipulation.java
+++ Java/Java-Programs/BitManipulation.java
@@ -44,6 +44,44 @@ public class BitManipulation {
 
     }
 
+    public static int clearIBits(int n, int i) {
+        int bitMask = (~0) << i;
+        System.out.println(n & bitMask);
+        return n & bitMask;
+    }
+
+    public static int clearRangeOfIBits(int n, int i, int j) {
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - i;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    public static boolean powerOfTwo(int n) {
+        // if ((n & (n - 1)) == 0) {
+        // System.out.println(n + " is Power of 2");
+        // return true;
+        // } else {
+        // System.out.println(n + " is not power of 2");
+        // return false;
+        // }
+        return (n & (n - 1)) == 0;
+
+    }
+
+    public static int countsBit(int n) {
+        int count = 0;
+        while (n > 0) {
+
+            if ((n & 1) == 1) {
+                count += 1;
+            }
+
+            n = n >> 1;
+        }
+        return count;
+    }
+
     public static void main(String args[]) {
 
         // bitwise And &
@@ -59,6 +97,7 @@ public class BitManipulation {
         // System.out.println((~5));
         // System.out.println((~6));
         // System.out.println((~(0)));
+        // System.out.println((~(1)));
         // System.out.println((~(-1)));
 
         // Binary Left shift
@@ -75,7 +114,14 @@ public class BitManipulation {
 
         // clearIthBit(3, 1);
 
-        System.out.println(updateIthBit(12, 0, 1));
+        // System.out.println(updateIthBit(12, 0, 1));
 
+        // System.out.println(clearIBits(15, 3));
+
+        // System.out.println(clearRangeOfIBits(15, 1, 2));
+
+        // System.out.println(powerOfTwo(18));
+
+        System.out.println(countsBit(15));
     }
 }

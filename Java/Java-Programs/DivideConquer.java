05714849b4faa8a7d8f1a8aa38da5cde82d24315
Prashastha <poojaryprashastha@gmail.com>
2025-11-11 22:55:10 +0530

    update

---
+++ Java/Java-Programs/DivideConquer.java
@@ -0,0 +1,64 @@
+public class DivideConquer {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void merge(int arr[], int si, int ei, int mid) {
+        int temp[] = new int[ei - si + 1];
+        int i = si; // iterator for left
+        int j = mid + 1; // iterator for right
+        int k = 0; // iterator for temp
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left part
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // right part
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // copy temp to original arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void mergeSort(int arr[], int si, int ei) {
+
+        if (si >= ei) {
+            return;
+        }
+
+        // work
+        int mid = si + (ei - si) / 2; // mid=(si+ei)/2
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid + 1, ei); // right part
+        merge(arr, si, ei, mid);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        printArr(arr);
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
+
+    }
+
+}

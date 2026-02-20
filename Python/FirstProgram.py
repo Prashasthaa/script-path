1a73a508cdc56e49924f8fbe19b358334972fbcc
Prashastha <poojaryprashastha@gmail.com>
2026-02-20 14:47:55 +0530

    hello python

---
+++ Python/FirstProgram.py
@@ -0,0 +1,72 @@
+# Print Statement
+print("Hello World","line should print in same line")
+print("This should print in new line")
+
+print (4+5)
+
+# Variables
+name="Prashastha" #string
+age=53 #number
+price=25.3 #decimal
+
+print(name,price,age)
+
+print("My name is ", name)
+
+#Data Types
+print(type(name))
+print(type(age))
+print(type(price))
+
+old=False
+num=None
+
+print(type(old))
+print(type(num))
+
+#Python is a case sensitive
+
+#print sum of two numbers
+num1=2
+num2=5
+sum=num1+num2
+print(sum)
+
+# operators
+
+a=5
+b=4
+
+#arthematic operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #modulo
+print(a**b) #power
+
+#relational operator
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
+#logical operator
+print(not False)
+print(not True)
+
+val1=True
+val2=True
+
+print(val1 and val2)
+print(val1 or val2)
+
+name=input("enter your name: ")
+print(name)
+
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+print(num2+num1)
+
+side=int(input("Enter the side"))
+print("Area = ",side*side)

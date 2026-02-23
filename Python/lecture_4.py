542b3cb1fa4bb12e22488280f9c8f00c894eca95
Prashastha <poojaryprashastha@gmail.com>
2026-02-23 18:18:19 +0530

    dictionary

---
+++ Python/lecture_4.py
@@ -0,0 +1,24 @@
+# Dicitionary & Set
+
+# Dicitonary
+
+# no order in dictionary
+# mutable
+# don't allow duplicate keys
+
+info={
+    "key":"value",
+    "name":"apnacollege",
+    "learning":"coding",
+    "age":35,
+    "is_adult":True,
+    "mark":67.7,
+    "subject":["pyton","C","Java"],
+    "topics":("dict","set"),
+    12:12,
+    12.2:12.2,
+}
+
+print(info)
+print(type(info))
+

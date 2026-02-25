aa76214356067c4c911c4181750a06701b5707d0
Prashastha <poojaryprashastha@gmail.com>
2026-02-25 17:43:06 +0530

    for loops

--- Python/lecture_5.py
+++ Python/lecture_5.py
@@ -69,3 +69,77 @@ while i<=5:
         continue
     print(i)
     i+=1
+
+# Loops
+
+# for loops
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+for val in nums:
+    print(val)
+
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+for val in nums:
+    print(val)
+else:
+    print("End")
+
+ums=(1,4,9,16,25,36,49,64,81,100)
+i=0
+x=25
+for val in nums:
+    if(val==x):
+        print("Found at ",i)
+    i+=1;    
+
+# Range
+# returns sequence of numbers
+seq=range(5)
+print(seq[1])
+print(seq[2])
+print(seq[3])
+print(seq[4])
+print(seq[0])
+
+for i in range(2,10,2):
+    print(i)
+
+# Practice
+# 
+# Q1.
+
+for i in range(100):
+    print(i)
+
+# Q2.
+
+for i in range(100,0,-1):
+    print(i)
+
+# Q3.
+# n=int(input("Enter a number:"))
+n=9
+for i in range(1,11):
+    print(n ,"*",i,"=",i*n)
+
+
+# Pass Statement
+
+for i in range(20):
+    pass
+
+print("Pass statement ")
+
+# Q4.
+n=5
+sum=0
+for i in range(n+1):
+   sum+=i
+print(sum)
+
+fact=1
+for i in range(1,n):
+    fact*=i
+
+print(fact)

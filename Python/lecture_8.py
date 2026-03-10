60d077cbe6191a897ede357dfbe148d2f04c359a
Prashastha <poojaryprashastha@gmail.com>
2026-03-10 14:21:46 +0530

    OOP in Python

--- Python/lecture_8.py
+++ Python/lecture_8.py
@@ -57,3 +57,71 @@ s3.welcome()
 print(s3.get_marks())
     
 
+# Practice Question
+# Q1.
+
+print("------------Practice Questions---------------")
+
+class Student4:
+    def __init__(self,name,m1,m2,m3):
+        self.name=name
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+        
+    def average_marks(self):
+        avg=(self.m1+self.m2+self.m3)/3
+        print("Average marks = ",avg)
+
+
+
+s5=Student4("Sanju",45,76,98)
+s5.average_marks()
+
+class Student1:
+    def __init__(self):
+        pass
+
+
+    @staticmethod #decorator
+    def hello():
+        print("hello this is static method")
+
+
+s3=Student1()
+s3.hello()
+
+# Abstraction
+# Hidding the implementation deatils of a class and only showing the essential features to the user
+
+# Encapsulation
+# Wrapping data and functions into a single unit(object).
+
+
+class Account:
+    def __init__(self, bal,acc):
+        self.balance=bal
+        self.accounut_no=acc
+
+    def debit(self,amount):
+        self.balance-=amount
+        print("Rs.",amount," was debited")
+        print("Total balance=",self.get_balance())
+
+    def credit(self,amount):
+        self.balance+=amount
+        print("Rs.",amount," was Credited")
+        print("Total balance=",self.get_balance())
+
+    
+    def get_balance(self):
+        return self.balance
+
+
+acc1=Account(11000,12134)
+print(acc1.balance,acc1.accounut_no)
+acc1.credit(2423)
+acc1.debit(333)
+print("Total balance=",acc1.get_balance())
+
+
